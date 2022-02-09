4c5d9a63a4b5cca93678fdc5d3a9123e0ab02d29
Todd W Crone <twcrone@gmail.com>
2022-02-08 20:13:27 -0500

    Add libraries to classpath

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ cd asm-agent
 mvn clean install
 cd ../asm-main
 ./gradlew clean build
-java -javaagent:"../asm-agent/target/asmplay-1.0.jar" -classpath app/libs -jar app/build/libs/app.jar
+cd ..
+java -javaagent:"asm-agent/target/asmplay-1.0.jar" -cp asm-main/app/build/libs/app.jar:asm-lib/lib/build/libs/lib.jar asm.main.App
